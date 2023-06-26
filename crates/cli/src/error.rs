5b3bf7769eeb43a7547f9456d97607429d62ec3b
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-06-26 15:24:16 -0500

    feat: implement concrete removal
    This is a small feature testing blueprint manipulation. The cli is now
    capable of stripping concrete from a blueprint book.

--- crates/cli/src/error.rs
+++ crates/cli/src/error.rs
@@ -1,7 +1,11 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error(transparent)]
-    Anyhow(#[from] anyhow::Error),
+    FactorioBlueprint(#[from] factorio_blueprint::Error),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+    #[error(transparent)]
+    Utf8(#[from] std::string::FromUtf8Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,8 +1,9 @@
 use crate::{
-    commands::{Commands, HelloCommands},
+    commands::{BlueprintCommands, Commands},
     error::Result,
 };
 use clap::Parser;
+use factorio_blueprint::{BlueprintCodec, Container};
 
 mod commands;
 mod error;
@@ -13,18 +14,91 @@ async fn main() -> Result<()> {
 
     let args = commands::Args::parse();
     match args.command {
-        Commands::Hello(hello) => {
-            let cmd = hello.command;
-            match cmd {
-                HelloCommands::World => {
-                    println!("Hello, world!");
-                }
-                HelloCommands::Name { name } => {
-                    println!("Hello, {}!", name);
-                }
+        Commands::Blueprint(blueprint) => match blueprint.command {
+            BlueprintCommands::GenerateBook(generate_book) => {
+                println!("Generating book from {}", generate_book.book.display());
+
+                let book_data = tokio::fs::read(&generate_book.book).await?;
+                let book_string = String::from_utf8(book_data)?;
+
+                let container = BlueprintCodec::decode_string(&book_string)?;
+                let no_concrete_container = rewrite_container(container.clone())?;
+                println!("{container:#?}");
+
+                let book_string = BlueprintCodec::encode_string(&no_concrete_container)?;
+                let book_data = book_string.into_bytes();
+                tokio::fs::write(&generate_book.output, book_data).await?;
             }
-        }
+            BlueprintCommands::Display(display) => {
+                println!("Displaying {}", display.blueprint.display());
+
+                let blueprint_data = tokio::fs::read(&display.blueprint).await?;
+                let blueprint_string = String::from_utf8(blueprint_data)?;
+
+                let container = BlueprintCodec::decode_string(&blueprint_string)?;
+
+                // Display - the container type, the label, and description.
+                match container {
+                    Container::BlueprintBook(book) => {
+                        println!("Loaded blueprint book ({:?})", book.label);
+                    }
+                    Container::Blueprint(_) => todo!(),
+                    Container::UpgradePlanner(_) => todo!(),
+                    Container::DeconstructionPlanner(_) => todo!(),
+                };
+            }
+        },
     }
 
     Ok(())
+}
+
+fn rewrite_container(container: Container) -> Result<Container> {
+    match container {
+        Container::BlueprintBook(book) => rewrite_book(book),
+        Container::Blueprint(blueprint) => rewrite_blueprint(blueprint),
+        Container::UpgradePlanner(_upgrade_planner) => todo!(),
+        Container::DeconstructionPlanner(_deconstruction_planner) => todo!(),
+    }
+}
+
+/// Creates a new blueprint book.
+/// This book should container two sub-books.
+/// The first is the original book, and the second is the modified book.
+/// The modified book should be the same as the original, but with no concrete.
+fn rewrite_book(book: factorio_blueprint::objects::BlueprintBook) -> Result<Container> {
+    let mut book = book;
+
+    // iterate over the blueprints book, and modify each blueprint.
+    let blueprints = book
+        .blueprints
+        .iter()
+        .map(|bp_value| {
+            // bp_value.item is a container, and must be called recursively.
+            let mut bp = bp_value.clone();
+            let bp_container = rewrite_container(bp.item).unwrap();
+            bp.item = bp_container;
+            bp
+        })
+        .collect();
+
+    book.blueprints = blueprints;
+
+    Ok(Container::BlueprintBook(book))
+}
+
+/// Replaces the blueprint with a new blueprint that has no concrete.
+fn rewrite_blueprint(blueprint: factorio_blueprint::objects::Blueprint) -> Result<Container> {
+    let mut blueprint = blueprint;
+    // iterate over the tiles, and remove any concrete.
+    let new_titles = blueprint
+        .tiles
+        .iter()
+        .filter(|tiles| tiles.name != "concrete")
+        .cloned()
+        .collect();
+
+    blueprint.tiles = new_titles;
+
+    Ok(Container::Blueprint(blueprint))
 }
