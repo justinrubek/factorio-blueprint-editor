06057fd6533bce4274a6dfa357f78b05798b3f68
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-06-26 15:24:16 -0500

    feat: implement concrete removal
    This is a small feature testing blueprint manipulation. The cli is now
    capable of stripping concrete from a blueprint book.

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -7,20 +7,39 @@ pub(crate) struct Args {
 
 #[derive(clap::Subcommand, Debug)]
 pub(crate) enum Commands {
-    Hello(Hello),
+    Blueprint(Blueprint),
 }
 
 #[derive(clap::Args, Debug)]
-pub(crate) struct Hello {
+pub(crate) struct Blueprint {
     #[clap(subcommand)]
-    pub command: HelloCommands,
+    pub command: BlueprintCommands,
 }
 
 #[derive(clap::Subcommand, Debug)]
-pub(crate) enum HelloCommands {
-    World,
-    Name {
-        #[arg()]
-        name: String,
-    },
+pub(crate) enum BlueprintCommands {
+    /// Creates a new blueprint book.
+    /// The book will have variations on the blueprints you provide.
+    /// For example, one variant would have all concrete removed.
+    /// Another may have upgraded belts.
+    GenerateBook(GenerateBook),
+    /// Display the top-level information about a blueprint.
+    Display(Display),
+}
+
+#[derive(clap::Args, Debug)]
+pub(crate) struct GenerateBook {
+    /// The blueprint book to modify.
+    #[clap(long, short)]
+    pub book: std::path::PathBuf,
+    /// the file to save the book to.
+    #[clap(long, short)]
+    pub output: std::path::PathBuf,
+}
+
+#[derive(clap::Args, Debug)]
+pub(crate) struct Display {
+    /// The blueprint to display.
+    #[clap(long, short)]
+    pub blueprint: std::path::PathBuf,
 }
